efeaa5dda3135f3e2fa4bc1b2fbf7c2d045f647c
yankeexe <yankee.exe@gmail.com>
2017-08-19 13:05:37 +0545

    Show Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,32 @@
 class ItemsController < ApplicationController
+	before_action :find_item, only: [:show, :edit, :update, :destroy]
+	
+	def index 
+		@items = Item.all.order("created_at DESC")
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create 
+		@item = Item.new(item_params)
+		if @item.save
+			redirect_to @item
+			else 
+			render 'new'
+		end
+	end
+
+	private
+	def item_params
+		params.require(:item).permit(:title, :description)
+	end
+
+	def find_item 
+		@item = Item.find(params[:id])
+	end
+
+
+
 end
